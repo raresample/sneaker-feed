1496cea02a28b30c471b22e55bb65f19ce1ed44b
Aaron Hope <aghopecode@gmail.com>
2023-06-01 19:48:05 -0700

    index.js image placement

--- pages/index.js
+++ pages/index.js
@@ -69,26 +69,15 @@ export default function Home() {
         const image = product.images.split("|")[0];
         const productUrl = `/products/${product.sku}`
         return (
-<div className="mb-8 flex justify-center" key={product.sku}>
-  <a
-    href={productUrl}
-    className="w-full flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-  >
-    <div className="relative flex-shrink-0 h-80 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg overflow-hidden">
-      <img
-        className="object-fit w-full h-full"
-        style={{ objectPosition: "center -16px" }}
-        src={`${image}`}
-        alt=""
-      />
-    </div>
-    <div className="flex flex-col flex-grow p-4 leading-normal">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">We could put a brief product description here.</p>
-    </div>
-  </a>
-</div>
-
+          <div className="mb-8 flex justify-center" key={product.sku}>
+            <a href={productUrl} className="w-full flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={`${image}`} alt="" />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">We could put a brief product description here.</p>
+              </div>
+            </a>
+          </div>
         );
       })}
     </main>
