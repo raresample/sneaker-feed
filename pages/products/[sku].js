5317bb23516e0020fc2c2e9d32ac9688f9428a3d
Aaron Hope <aghopecode@gmail.com>
2023-06-01 18:04:01 -0700

    [sku] alignment

--- pages/products/[sku].js
+++ pages/products/[sku].js
@@ -31,13 +31,16 @@ export default function Home() {
       {dataResponse.map((product) => {
         const image = product.images.split("|")[0];
         return (
-          <div className="m-4" key={product.sku}>
-            <h1 className="text-3xl font-bold">{product.title}</h1>
-            <h2 className="text-2xl">{product.detail}</h2>
-            <h2 className="text-2xl mb-2">{product.price}</h2>
-            <h2 className="text-2xl"><a href={`${product.url}`} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" target="_blank">Buy Now</a></h2>
-            <div className="mt-6">
-              <img src={`${image}`} alt="" />
+          <div id="product" className="m-4 mx-auto max-w-2xl" key={product.sku}>
+            <div className="">
+              <h1 className="text-3xl font-bold">{product.title}</h1>
+              <h2 className="text-2xl">{product.detail}</h2>
+              <h2 className="text-2xl mb-2">{product.price}</h2>
+              <h2 className="text-2xl"><a href={`${product.url}`} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" target="_blank">Buy Now</a></h2>
+            </div>
+            
+            <div className="mt-6 relative image-container">
+              <img src={`${image}`} alt="" className="product-image" />
             </div>
           </div>
         );
