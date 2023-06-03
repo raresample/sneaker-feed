771c4f27c2ae9d835e6fcda9a2e70a73e1b04a05
Aaron Hope <aghopecode@gmail.com>
2023-06-02 18:12:22 -0700

    Orange navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-gray-800 py-4 z-10">
+    <nav className="fixed top-0 left-0 right-0 bg-orange-500 py-4 z-10">
       <div className="container mx-auto px-4">
         <Link href="/" className="text-white font-bold text-lg">sneaker FEED</Link>
       </div>
