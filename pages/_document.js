b01b25a67b723fb0c6377def7fc5064b9a1a056d
Aaron Hope <aghopecode@gmail.com>
2023-06-02 19:18:12 -0700

    Updated page title

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Aaron Hope's Sneaker Feed v1</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
