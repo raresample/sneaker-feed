7386c2da33f62e526980e952a9a10d7503fc45c3
Aaron Hope <aghopecode@gmail.com>
2023-06-18 19:40:00 -0700

    Fixed index-paginated card widths

--- pages/index-paginated.js
+++ pages/index-paginated.js
@@ -31,7 +31,7 @@ export default function Home() {
             const productUrl = `/products/${product.sku}`
             return (
               <><div className="mb-8 flex justify-center" key={product.sku}>
-                <a href={productUrl} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <a href={productUrl} className="w-full flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                   <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={`${image}`} alt="" />
                   <div className="flex flex-col justify-between p-4 leading-normal">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
